3b8e33cb4148e1542d64c940eebbdd0a971dc27d
jcy1511 <jcy1511@gmail.com>
2020-12-10 00:38:29 +0900

    def okcancel()

--- gui4.py
+++ gui4.py
@@ -21,9 +21,14 @@ def warn():
 def error():
     msgbox.showerror("에러", "결제 오류가 발생했습니다.")
 
+def okcancel():
+    msgbox.askokcancel("확인/취소", "확인/취소")
+
+
 Button(root, text="알림", command=info).pack()
 Button(root, text="경고", command=warn).pack()
 Button(root, text="에러", command=error).pack()
+Button(root, text="확인/취소", command=okcancel).pack()
 
 
 root.mainloop()
