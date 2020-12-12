6558074d654e67ef0f30c9bd3c18632ba10882ce
jcy1511 <jcy1511@gmail.com>
2020-12-12 23:29:29 +0900

    Update gui_notepad.py

---
+++ codes/gui_notepad.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+root = Tk()
+root.title("gui2")
+root.geometry("400x400")
+
+menu = Menu(root)
+
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="열기")
+menu_file.add_command(label="저장")
+menu_file.add_separator()
+menu_file.add_command(label="끝내기")
+menu.add_cascade(label="파일", menu=menu_file)
+
+menu.add_cascade(label="편집")
+menu.add_cascade(label="서식")
+menu.add_cascade(label="보기")
+menu.add_cascade(label="도움말")
+
+root.config(menu=menu)
+
+
+text=Text(root)
+text.pack(fill="both", expand=True)
+
+
+root.mainloop()
