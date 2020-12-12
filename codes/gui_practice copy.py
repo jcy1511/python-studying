1f46c3176818f09f575e5988da6bf54470bd4b56
jcy1511 <jcy1511@gmail.com>
2020-12-12 23:09:43 +0900

    Update gui_practice copy.py

--- codes/gui_practice copy.py
+++ codes/gui_practice copy.py
@@ -4,4 +4,10 @@ root = Tk()
 root.title("gui2")
 root.geometry("400x400")
 
+btn1 = Button(root, text="버튼1")
+btn2 = Button(root, text="버튼2")
+
+btn1.grid(row=0, column=0)
+btn2.grid(row=11, column=12)
+
 root.mainloop()
