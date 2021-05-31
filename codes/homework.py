91e4e35a1b28b4630978d3be0f0060b048f6b0f0
jcy1511 <jcy1511@gmail.com>
2021-05-31 22:54:54 +0900

    계수 찾는 기능
    계수 찾는 기능 만듦

--- codes/homework.py
+++ codes/homework.py
@@ -1,4 +1,13 @@
-def f(x) :
-    return(x^2 + 2*x + 1)
+A = input("f(x) = ")        # f(x)를 A로 정의
 
-print(f(1))
+plus1 = A.index("+")        # + 기호의 위치를 찾음
+plus2 = A.index("+", plus1+1)
+
+a = A[:plus1-3]         # 이차항의 계수 a
+if a == "":     # 이차항의 계수가 1이라서 생략되어 있을 때 이차항의 계수 변수인 a를 1로 설정해주는 코드
+    a = 1  
+
+b = A[plus1+1:plus2-1]      # 일차항의 계수 b
+c = A[plus2+1:]         # 상수항의 계수 c
+
+print(a,b,c)
